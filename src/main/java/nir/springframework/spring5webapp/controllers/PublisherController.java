133decda7b70785fa17156d81f31eb09d0435aa3
Simkin <nir.simkin@gmail.com>
2019-11-29 17:55:19 +0200

    thymeleaf implementation example with spring MVC

---
+++ src/main/java/nir/springframework/spring5webapp/controllers/PublisherController.java
@@ -0,0 +1,23 @@
+package nir.springframework.spring5webapp.controllers;
+
+import nir.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PublisherController {
+
+    private PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    @RequestMapping("/publishers")
+    public String getPublishers(Model model) {
+        model.addAttribute("publishers", publisherRepository.findAll());
+
+        return "publishers";
+    }
+}
